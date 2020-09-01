f3730ad167074c2eae316f597f17b5baf597b364
qntm <qntm@users.noreply.github.com>
2020-09-01 12:29:38 +0100

    Lint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+/* global BigInt */
+
 const bar = '\u0305'
 
 const banks = [
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,6 +1,7 @@
 'use strict'
 
 /* eslint-env jasmine */
+/* global BigInt */
 
 const {
   arabicToRoman,
